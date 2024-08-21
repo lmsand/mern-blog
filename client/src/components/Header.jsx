dc07b47d20b9d7e620a7aaef16a0d15c0f39e847
lmsand <lmsand99@icloud.com>
2024-08-21 01:12:48 -0500

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
         </Button>
 
         <Link>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='purpleToBlue' outline>
             Sign In
           </Button>
         </Link>
