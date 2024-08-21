ac9c6b03d51271c09c975ce0eca5818f81bfc3a9
lmsand <lmsand99@icloud.com>
2024-08-21 15:47:35 -0500

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controller.js'
+import { signin, signup } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
 // post, when you want to create something
 router.post('/signup', signup)
+router.post('/signin', signin)
 
 export default router
